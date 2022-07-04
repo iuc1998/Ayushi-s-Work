78c6bf584443a8a69927de9e276410d4871c0afe
iuc1998 <104521904+iuc1998@users.noreply.github.com>
2022-07-05 04:36:07 +0530

    Add files via upload

---
+++ ClavaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClavaxApp.swift
+//  Clavax
+//
+//  Created by Ayushi Bhatnagar on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ClavaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
